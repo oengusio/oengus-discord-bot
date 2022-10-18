9aacf77c8b7d5eebd9c689942c1f407f3cdb289d
duncte123 <contact@duncte123.me>
2022-10-18 20:56:44 +0200

    Add marathon discord settings

--- api/oengus.go
+++ api/oengus.go
@@ -1,12 +1,20 @@
 package api
 
 type MarathonStats struct {
-    SubmissionCount int    `json:"submissionCount"`
-    RunnerCount     int    `json:"runnerCount"`
-    TotalLength     string `json:"totalLength"`
-    AverageEstimate string `json:"averageEstimate"`
+	SubmissionCount int    `json:"submissionCount"`
+	RunnerCount     int    `json:"runnerCount"`
+	TotalLength     string `json:"totalLength"`
+	AverageEstimate string `json:"averageEstimate"`
+}
+
+type MarathonDiscordSettings struct {
+	GuildId                string `json:"guild_id"`
+	RunnerRoleId           string `json:"runner_role_id"`
+	DonationChannel        string `json:"donation_channel"`
+	SubmissionChannel      string `json:"submission_channel"`
+	SubmissionAuditChannel string `json:"submission_audit_channel"`
 }
 
 func getMarathonStats(marathonId string) MarathonStats {
-    return MarathonStats{} // TODO
+	return MarathonStats{} // TODO
 }
