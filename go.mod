aad4860c9726004cad826d8280366a3c5afe0700
duncte123 <contact@duncte123.me>
2022-10-23 13:42:00 +0200

    Switch to using the database instead of using the web api

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module oenugs-bot
 go 1.16
 
 require (
-	github.com/bwmarrin/discordgo v0.23.3-0.20211117035633-fd6228c0d536 // indirect
-	github.com/jackc/pgx/v4 v4.17.2 // indirect
+	github.com/bwmarrin/discordgo v0.23.3-0.20211117035633-fd6228c0d536
+	github.com/jackc/pgx/v4 v4.17.2
 )
--- role-assign.go
+++ role-assign.go
@@ -19,15 +19,93 @@ var guildMembersPageLimit = 1000
 
 // TODO: binary search the shit out of this
 
-func assignRoleToRunnersESA(s *discordgo.Session) {
+func assignRoleToRunnersESA(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	//assignRolesToRunners(s, "ESA-Win23", EsaDiscord, esaRunnerRole)
-	assignRolesToRunners(s, "caching", EsaDiscord, esaRunnerRole)
+	assignRolesToRunners(s, "poggers", EsaDiscord, esaRunnerRole)
+
+	go func() {
+		ids, _ := api.GetAcceptedRunnerDiscordIds("poggers")
+
+		for _, id := range ids {
+			fakeUser := discordgo.User{
+				ID: id,
+			}
+
+			s.ChannelMessageSend(i.ChannelID, fakeUser.Mention())
+		}
+	}()
 }
 func assignRoleToRunnersBSG(s *discordgo.Session) {
 	assignRolesToRunners(s, "", BsgDiscord, bsgRunnerRole)
 }
 
-func removeRoleFromRunners(s *discordgo.Session, marathonId string, guildId string, roleId string) {
+func memberHasRole(member *discordgo.Member, roleId string) bool {
+	for _, mRole := range member.Roles {
+		if mRole == roleId {
+			return true
+		}
+	}
+
+	return false
+}
+
+func assignRolesToRunners(s *discordgo.Session, marathonId string, guildId string, roleId string) {
+	// TODO
+	//  1. Fetch marathon settings (already done before this func)
+	//  2. Fetch submissions with status: VALIDATED, BACKUP, BONUS
+	//  3. Assign role
+	//  4. log feedback (audit channel??) about runners that could not be assigned a role with reason (no perms or no discord id)
+	//      - what channel to log in when audit log is not set? (use command??)
+
+	go startRoleAssignment(s, marathonId, guildId, roleId)
+}
+
+func startRoleAssignment(s *discordgo.Session, marathonId, guildId, roleId string) {
+	userIDs, err := api.GetAcceptedRunnerDiscordIds("poggers")
+
+	if err != nil {
+		fmt.Println("Failed to load users for marathon", marathonId, err)
+		return
+	}
+
+	for _, userId := range userIDs {
+		applyRoleToUser(s, userId, guildId, roleId)
+	}
+
+	fmt.Println("Done!")
+}
+
+func findMember(s *discordgo.Session, guildId, userId string) *discordgo.Member {
+	member, err := s.GuildMember(guildId, userId)
+
+	if err == nil {
+		return member
+	}
+
+	return nil
+}
+
+func applyRoleToUser(s *discordgo.Session, userId, guildId, roleId string) {
+	member := findMember(s, guildId, userId)
+
+	if member == nil {
+		fmt.Println("Null member for", userId)
+		return
+	}
+
+	if memberHasRole(member, roleId) {
+		fmt.Println(member.User.String(), "already has the role, skipping")
+		return
+	}
+
+	err := s.GuildMemberRoleAdd(guildId, userId, roleId)
+
+	if err != nil {
+		fmt.Println("applying role to", member.User.String(), "failed", err)
+	}
+}
+
+func removeRoleFromRunners(s *discordgo.Session, marathonId, guildId, roleId string) {
 	guild, err := lookupGuild(s, guildId)
 
 	if err != nil {
@@ -58,109 +136,4 @@ func removeRoleFromRunners(s *discordgo.Session, marathonId string, guildId stri
 			}
 		}
 	}()
-}
-
-func memberHasRole(member *discordgo.Member, roleId string) bool {
-	for _, mRole := range member.Roles {
-		if mRole == roleId {
-			return true
-		}
-	}
-
-	return false
-}
-
-func assignRolesToRunners(s *discordgo.Session, marathonId string, guildId string, roleId string) {
-	// TODO
-	//  1. Fetch marathon settings (already done before this func)
-	//  2. Fetch submissions with status: VALIDATED, BACKUP, BONUS
-	//  3. Assign role
-	//  4. log feedback (audit channel??) about runners that could not be assigned a role with reason (no perms or no discord id)
-	//      - what channel to log in when audit log is not set?
-
-	guild, err := lookupGuild(s, guildId)
-
-	if err != nil {
-		fmt.Println("Failed to look up guild for marathon ", marathonId, guildId, err)
-		return
-	}
-
-	fmt.Println(guild.Members, len(guild.Members))
-
-	go startRoleAssignment(marathonId, s, guild, roleId)
-}
-
-// TODO: look up user id with tag from member cache
-
-func startRoleAssignment(marathonId string, s *discordgo.Session, guild *discordgo.Guild, roleId string) {
-	submissions, err := api.FetchAcceptedRunners(marathonId)
-
-	if err != nil {
-		s.ChannelMessageSend(botTestingChannel, err.Error())
-		return
-	}
-
-	if guild.MemberCount > len(guild.Members) {
-		members, err := recursiveGuildMembers(s, guild.ID, "", guildMembersPageLimit)
-		if err != nil {
-			fmt.Printf("unable to query guild members: %s\n", err)
-			return
-		}
-
-		guild.Members = members
-		guild.MemberCount = len(members)
-	}
-
-	for _, submission := range submissions {
-		discordTag := findDiscordUsername(submission)
-
-		if discordTag == "" {
-			fmt.Printf("No discord for %s\n", submission.User.Username)
-			continue
-		}
-
-		member := findUser(discordTag, guild.Members)
-
-		if member == nil {
-			fmt.Printf("%s is not in guild (correct discord linked?)\n", discordTag)
-			continue
-		}
-
-		applyRoleToUser(s, member, guild.ID, roleId)
-	}
-
-	fmt.Println("Done!")
-}
-
-func findDiscordUsername(sub api.Submission) string {
-	for _, cnx := range sub.User.Connections {
-		if cnx.Platform == "DISCORD" {
-			return cnx.Username
-		}
-	}
-
-	return ""
-}
-
-func findUser(tag string, members []*discordgo.Member) *discordgo.Member {
-	for _, member := range members {
-		if member.User.String() == tag {
-			return member
-		}
-	}
-
-	return nil
-}
-
-func applyRoleToUser(s *discordgo.Session, member *discordgo.Member, guildId, roleId string) {
-	if memberHasRole(member, roleId) {
-		fmt.Println(member.User.String(), "already has the role, skipping")
-		return
-	}
-
-	err := s.GuildMemberRoleAdd(guildId, member.User.ID, roleId)
-
-	if err != nil {
-		fmt.Println("applying role to", member.User.String(), "failed", err)
-	}
 }
