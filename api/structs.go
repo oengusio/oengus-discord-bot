7a9c5e5f676dad3267f73e20ff73bfbe746dc646
duncte123 <contact@duncte123.me>
2025-02-04 19:40:10 +0100

    Finish announcing multiplayer runs
    Fixes https://github.com/oengusio/oengus-discord-bot/issues/5

--- api/structs.go
+++ api/structs.go
@@ -1,81 +1,81 @@
 package api
 
 type WebhookData struct {
-	Event              string         `json:"event"`
-	Url                string         `json:"url"`
-	Game               Game           `json:"game"`
-	Category           Category       `json:"category"`
-	Submission         Submission     `json:"submission"` // TODO: make DTOs for submissions
-	OriginalSubmission Submission     `json:"original_submission"`
-	DeletedBy          User           `json:"deleted_by"`
-	Selections         []SelectionDto `json:"selections"`
+    Event              string         `json:"event"`
+    Url                string         `json:"url"`
+    Game               Game           `json:"game"`
+    Category           Category       `json:"category"`
+    Submission         Submission     `json:"submission"` // TODO: make DTOs for submissions
+    OriginalSubmission Submission     `json:"original_submission"`
+    DeletedBy          User           `json:"deleted_by"`
+    Selections         []SelectionDto `json:"selections"`
 }
 
 type BotHookParams struct {
-	EditSub    string
-	NewSub     string
-	Donation   string
-	MarathonId string
+    EditSub    string
+    NewSub     string
+    Donation   string
+    MarathonId string
 }
 
 type MarathonStats struct {
-	SubmissionCount int    `json:"submissionCount"`
-	RunnerCount     int    `json:"runnerCount"`
-	TotalLength     string `json:"totalLength"`
-	AverageEstimate string `json:"averageEstimate"`
+    SubmissionCount int    `json:"submissionCount"`
+    RunnerCount     int    `json:"runnerCount"`
+    TotalLength     string `json:"totalLength"`
+    AverageEstimate string `json:"averageEstimate"`
 }
 
 // TODO: Might store this as json in the database tbh
 type MarathonSettings struct {
-	Discord MarathonDiscordSettings `json:"discord_settings"`
+    Discord MarathonDiscordSettings `json:"discord_settings"`
 }
 
 type MarathonDiscordSettings struct {
-	GuildId                string `json:"guild_id"`
-	RunnerRoleId           string `json:"runner_role_id"`
-	DonationChannel        string `json:"donation_channel"`
-	SubmissionChannel      string `json:"submission_channel"`
-	SubmissionAuditChannel string `json:"submission_audit_channel"`
+    GuildId                string `json:"guild_id"`
+    RunnerRoleId           string `json:"runner_role_id"`
+    DonationChannel        string `json:"donation_channel"`
+    SubmissionChannel      string `json:"submission_channel"`
+    SubmissionAuditChannel string `json:"submission_audit_channel"`
 }
 
 type SelectionResponse map[string]struct {
-	Id         int    `json:"id"`
-	CategoryId int    `json:"categoryId"`
-	Status     string `json:"status"`
+    Id         int    `json:"id"`
+    CategoryId int    `json:"categoryId"`
+    Status     string `json:"status"`
 }
 
 type SubmissionResponse struct {
-	Content     []Submission `json:"content"`
-	TotalPages  int          `json:"totalPages"`
-	CurrentPage int          `json:"currentPage"`
-	First       bool         `json:"first"`
-	Last        bool         `json:"last"`
-	Empty       bool         `json:"empty"`
+    Content     []Submission `json:"content"`
+    TotalPages  int          `json:"totalPages"`
+    CurrentPage int          `json:"currentPage"`
+    First       bool         `json:"first"`
+    Last        bool         `json:"last"`
+    Empty       bool         `json:"empty"`
 }
 
 type Submission struct {
-	Id        int        `json:"id"`
-	User      User       `json:"user"`
-	Games     []Game     `json:"games"`
-	Opponents []Opponent `json:"opponents"`
+    Id        int        `json:"id"`
+    User      User       `json:"user"`
+    Games     []Game     `json:"games"`
+    Opponents []Opponent `json:"opponents"`
 }
 
 type Game struct {
-	Id          int        `json:"id"`
-	Name        string     `json:"name"`
-	Console     string     `json:"console"`
-	Description string     `json:"description"`
-	Categories  []Category `json:"categories"`
+    Id          int        `json:"id"`
+    Name        string     `json:"name"`
+    Console     string     `json:"console"`
+    Description string     `json:"description"`
+    Categories  []Category `json:"categories"`
 }
 
 type Category struct {
-	Id          int    `json:"id"`
-	Name        string `json:"name"`
-	Estimate    string `json:"estimate"`
-	Description string `json:"description"`
-	Video       string `json:"video"`
-	Type        string `json:"type"`
-	Status      string `json:"status"`
+    Id          int    `json:"id"`
+    Name        string `json:"name"`
+    Estimate    string `json:"estimate"`
+    Description string `json:"description"`
+    Video       string `json:"video"`
+    Type        string `json:"type"`
+    Status      string `json:"status"`
 }
 
 /*
@@ -89,22 +89,21 @@ type Category struct {
 */
 
 type Opponent struct {
-	Id           int    `json:"id"`
-	CategoryId   int    `json:"categoryId"`
-	UserId       int    `json:"userId"`
-	SubmissionId int    `json:"submissionId"`
-	Video        string `json:"video"`
+    Id           int    `json:"id"`
+    CategoryId   int    `json:"categoryId"`
+    UserId       int    `json:"userId"`
+    SubmissionId int    `json:"submissionId"`
+    Video        string `json:"video"`
 }
 
 // NOTE: we're only storing the most important bits
 type User struct {
-	Id          int              `json:"id"`
-	Username    string           `json:"username"`
-	Connections []UserConnection `json:"connections"`
+    Id          int              `json:"id"`
+    Username    string           `json:"username"`
+    Connections []UserConnection `json:"connections"`
 }
 
 type UserConnection struct {
-	Id       int    `json:"id"`
-	Platform string `json:"platform"`
-	Username string `json:"username"`
+    Platform string `json:"platform"`
+    Username string `json:"username"`
 }
